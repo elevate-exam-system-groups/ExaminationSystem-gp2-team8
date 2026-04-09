ef889ffa965e3d8c03548eca0d85203da95cea33
asalaehab824@gmail.com <asalaehab824@gmail.com>
2026-04-09 20:45:03 +0200

    .

---
+++ ExaminationSystem/Core/DomainLayer/Contracts/IGeneralRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface IGeneralRepository<TEntity>  where TEntity :class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IQueryable<TEntity>> GetAll();
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+
+    }
+}
---
+++ ExaminationSystem/Infrastructure/Persistance/Data/ExamApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Data
+{
+    public class ExamApplicationDbContext(DbContextOptions<ExamApplicationDbContext> options):DbContext(options)
+    {
+
+    }
+}
