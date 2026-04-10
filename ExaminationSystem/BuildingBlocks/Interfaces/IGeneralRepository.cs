1d1104238d7f2f973d8a213790c5da81b6f567f8
asalaehab824@gmail.com <asalaehab824@gmail.com>
2026-04-10 20:26:42 +0200

    GeneralRepository

---
+++ ExaminationSystem/BuildingBlocks/Interfaces/IGeneralRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ExaminationSystem.BuildingBlocks.Interfaces
+{
+    public interface IGeneralRepository<TEnity> where TEnity : class
+    {
+            Task<TEnity?> GetByIdAsync(int id);
+            IQueryable<TEnity> GetAll();
+            void Add(TEnity entity);
+            void Update(TEnity entity);
+            void Delete(TEnity entity);
+    }
+}
---
+++ ExaminationSystem/Infrastructure/Persistence/Repositories/GeneralRepository.cs
@@ -0,0 +1,25 @@
+﻿using ExaminationSystem.BuildingBlocks.Interfaces;
+
+namespace ExaminationSystem.Infrastructure.Persistence.Repositories
+{
+    public class GeneralRepository<TEntity> : IGeneralRepository<TEntity> where TEntity : class
+    {
+        private readonly ExamAppDbContext _dbcontext;
+
+        public GeneralRepository(ExamAppDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public void Add(TEntity entity)=> _dbcontext.Set<TEntity>().Add(entity);
+
+
+        public void Delete(TEntity entity) => _dbcontext.Set<TEntity>().Remove(entity);
+
+        public IQueryable<TEntity> GetAll() => _dbcontext.Set<TEntity>();
+
+        public async Task<TEntity?> GetByIdAsync(int id)=> await _dbcontext.Set<TEntity>().FindAsync(id);
+
+        public void Update(TEntity entity)=> _dbcontext.Set<TEntity>().Update(entity);
+        
+    }
+}
