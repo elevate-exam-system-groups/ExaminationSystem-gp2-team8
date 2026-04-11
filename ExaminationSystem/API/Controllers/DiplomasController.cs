2bd41ee1f56bbf474ed0e284ea1bdeecdd4b5ff5
asalaehab824@gmail.com <asalaehab824@gmail.com>
2026-04-11 14:44:11 +0200

    DiplomasController

---
+++ ExaminationSystem/API/Controllers/DiplomasController.cs
@@ -0,0 +1,22 @@
+﻿using ExaminationSystem.Features.Attempts.GetAttempts;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExaminationSystem.API.Controllers
+{
+    [Route("api/student/diplomas")]
+    [ApiController]
+    public class DiplomasController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DiplomasController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+     
+
+    }
+}
