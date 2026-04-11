4bb41b554ed9e6d932fd713664dcb4e54742037d
asalaehab824@gmail.com <asalaehab824@gmail.com>
2026-04-11 23:54:43 +0200

    ApiResponse

--- ExaminationSystem/API/Middlewares/ErrorToReturn.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExaminationSystem.API.Middlewares
-{
-    public class ErrorToReturn
-    {
-        public int ErrorCode { get; set; }
-        public string Message { get; set; }
-    }
-}
