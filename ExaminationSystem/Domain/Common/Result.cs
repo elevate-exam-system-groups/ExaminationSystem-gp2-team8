54023e0011e193a8d6a847379db5e56834453655
Hesham-Mohamed32 <hesham366mohamed@gmail.com>
2026-04-12 20:38:09 +0200

    implemented reset and forget password feature

---
+++ ExaminationSystem/Domain/Common/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace ExaminationSystem.Domain.Common
+{
+    public class Result
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+        public List<string> Errors { get; set; } = new();
+
+        public static Result Success(string message = "Success") 
+            => new Result { IsSuccess = true, Message = message };  
+
+        public static Result Fail(string message, List<string>? errors = null)
+            => new Result { IsSuccess = false, Message = message, Errors = errors ?? new List<string>() };
+    }
+} 
