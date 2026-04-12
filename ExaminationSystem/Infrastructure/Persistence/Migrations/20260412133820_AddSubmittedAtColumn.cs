eebc69222e72ee71b1938bcc0e8d3e800d2450a2
rahma2005 <rhmarhma360@gmail.com>
2026-04-12 15:40:33 +0200

    AddSubmittedAtColumn migration

---
+++ ExaminationSystem/Infrastructure/Persistence/Migrations/20260412133820_AddSubmittedAtColumn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExaminationSystem.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubmittedAtColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "score",
+                table: "Attempts",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SubmittedAt",
+                table: "Attempts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubmittedAt",
+                table: "Attempts");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "score",
+                table: "Attempts",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
