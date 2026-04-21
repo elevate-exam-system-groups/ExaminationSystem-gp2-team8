c682351b882035ab7eff49649308a2e984e3b3fa
asalaehab824@gmail.com <asalaehab824@gmail.com>
2026-04-21 20:51:14 +0200

    edit Admin

--- ExaminationSystem/Domain/Entities/Admin.cs
+++ ExaminationSystem/Domain/Entities/Admin.cs
@@ -2,5 +2,8 @@
 {
     public class Admin: User
     {
+        public ICollection<Diploma> CreatedDiplomas { get; set; } = new List<Diploma>();
+        public ICollection<Quiz> CreatedQuizzes { get; set; } = new List<Quiz>();
+        public ICollection<Question> CreatedQuestions { get; set; } = new List<Question>();
     }
 }
