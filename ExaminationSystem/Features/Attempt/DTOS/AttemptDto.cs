b535cf1b430280c6bbfd788af383ea29753a3aca
asalaehab824@gmail.com <asalaehab824@gmail.com>
2026-04-21 21:29:42 +0200

    fix attempt branch

--- ExaminationSystem/Features/Attempt/DTOS/AttemptDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExaminationSystem.Features.Attempt.DTOS
-{
-    public class AttemptDto
-    {
-        public int Id { get; set; }
-        public float Score { get; set; }
-        public string Status { get; set; } = string.Empty;
-    }
-}
--- ExaminationSystem/Features/Attempts/GetAttemptResult/ViewAttemptResultsHandler.cs
+++ ExaminationSystem/Features/Attempts/GetAttemptResult/ViewAttemptResultsHandler.cs
@@ -1,20 +1,18 @@
-using ExaminationSystem.BuildingBlocks.ExceptionHandling;
+﻿using ExaminationSystem.BuildingBlocks.ExceptionHandling;
 using ExaminationSystem.BuildingBlocks.Exceptions;
 using ExaminationSystem.BuildingBlocks.Interfaces;
-using ExaminationSystem.Domain.Entities;
 using ExaminationSystem.Domain.Enums;
-using ExaminationSystem.Features.Attempt.DTOS;
+using ExaminationSystem.Features.Attempts.DTOs;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace ExaminationSystem.Features.Attempt
+namespace ExaminationSystem.Features.Attempts.GetAttemptResult
 {
-    public record ViewAttemptResults(int studentId, int id) : IRequest<ApiResponse<ViewResultDTO>>;
-
     public class ViewAttemptResultsHandler : IRequestHandler<ViewAttemptResults, ApiResponse<ViewResultDTO>>
     {
-        IGeneralRepository<ExaminationSystem.Domain.Entities.Attempts> _repository;
-        public ViewAttemptResultsHandler(IGeneralRepository<ExaminationSystem.Domain.Entities.Attempts> repository)
+
+        IGeneralRepository<Domain.Entities.Attempts> _repository;
+        public ViewAttemptResultsHandler(IGeneralRepository<Domain.Entities.Attempts> repository)
         {
             _repository = repository;
         }
@@ -32,7 +30,7 @@ namespace ExaminationSystem.Features.Attempt
                             .ThenInclude(q => q.Questions)
                             .ThenInclude(o => o.Options)
                          .Where(a => a.Id == request.id && (a.Attempt == AttemptStatus.TimeOut || a.Attempt == AttemptStatus.Submit)
-                        && a.UserId==request.studentId )
+                        && a.UserId == request.studentId)
                          .FirstOrDefaultAsync(cancellationToken);
 
             if (attempt is null) throw new ForbiddenException(" results not available until submitted ");
@@ -40,11 +38,11 @@ namespace ExaminationSystem.Features.Attempt
 
             var Status = attempt.Quiz.PassScore <= attempt.score ? "Passed" : "Failed";
 
-            var questionsCount=attempt.Quiz.Questions.Count;
+            var questionsCount = attempt.Quiz.Questions.Count;
 
-            var correctCount=attempt.StudentAnswers.Count(a=>a.SelectedOption.IsCorrect==true);
+            var correctCount = attempt.StudentAnswers.Count(a => a.SelectedOption.IsCorrect == true);
 
-          
+
 
 
             var attemptDto = new ViewResultDTO()
@@ -54,7 +52,8 @@ namespace ExaminationSystem.Features.Attempt
                 TotalQuestions = questionsCount,
                 CorrectCount = correctCount,
                 // i wanna get list of questions for this attempt
-                Questions = attempt.StudentAnswers.Select(s =>  new QuestionAttemptDTO() {
+                Questions = attempt.StudentAnswers.Select(s => new QuestionAttemptDTO()
+                {
 
                     QuestionId = s.QuestionId,
                     studentAnswer = s.SelectedOption.OptionText,
@@ -65,9 +64,8 @@ namespace ExaminationSystem.Features.Attempt
 
             };
 
-            return  ApiResponse<ViewResultDTO>.SuccessResponse(attemptDto);
+            return ApiResponse<ViewResultDTO>.SuccessResponse(attemptDto);
         }
+
     }
-
-
 }
